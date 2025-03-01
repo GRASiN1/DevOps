0669e34c60bbd1a3d9b90e741aa65fd8f89de519
GRASiN1 <guaravrajsingh047@gmail.com>
2025-03-01 11:23:53 -0500

    learned how to make bucket items public in S3

--- boto3/app.py
+++ boto3/app.py
@@ -1,13 +1,13 @@
 import boto3
+import boto3
+region = 'ap-south-1'
 
-
-s3 = boto3.resource('s3')
+# s3 = boto3.resource('s3')
 # for bucket in s3.buckets.all():
 #     print(bucket.name)
 
 # s3 = boto3.client('s3')
-BUCKET_NAME='dev-ops-course-practice-bucket'
-region = 'ap-south-1'
+# BUCKET_NAME='dev-ops-course-practice-bucket'
 
 # sesponse = s3.create_bucket(
 #         Bucket=BUCKET_NAME,
@@ -18,4 +18,16 @@ region = 'ap-south-1'
 
 # s3.Bucket(BUCKET_NAME).delete()
 
-s3.Bucket(BUCKET_NAME).download_file('test.txt', 'text.txt')
+# s3.Bucket(BUCKET_NAME).download_file('test.txt', 'text.txt')
+
+# ssm = boto3.client('ssm', region_name=region)
+
+# response = ssm.get_parameter(Name='sample-parameter', WithDecryption=True)
+
+# print(response['Parameter']['Value'])
+
+import pandas
+
+df = pandas.read_csv('https://dev-ops-course-practice-bucket.s3.ap-south-1.amazonaws.com/test.csv')
+
+print(df.head())
